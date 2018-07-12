5763817256bc639c4ef506c6f36d53ae1da74b82
Naoyuki Miyazaki <miyazaki@buddying.jp>
2018-07-13 01:14:33 +0900

    rename

--- backup.sh
+++ backup.sh
@@ -3,5 +3,3 @@
 # TODO コンテナの確認をしないと空のファイルで上書きされてしまう
 docker exec sample-db /usr/bin/mysqldump -u root --password=passw0rd sample | gzip > init/init.sql.gz
 # docker exec sample-db /usr/bin/mysqldump -u root --password=passw0rd sample > init.sql
-
-docker-compose stop
